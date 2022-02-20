ac07e04c5781fa84adbdf1c0f312c26461894621
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 07:26:42 +0700

    #5 - Add mustacheCompiler Bean

--- src/main/java/com/hendisantika/SpringbootMustacheDemo1Application.java
+++ src/main/java/com/hendisantika/SpringbootMustacheDemo1Application.java
@@ -1,7 +1,11 @@
 package com.hendisantika;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SpringbootMustacheDemo1Application {
@@ -10,4 +14,18 @@ public class SpringbootMustacheDemo1Application {
         SpringApplication.run(SpringbootMustacheDemo1Application.class, args);
     }
 
+    @Bean
+    public Mustache.Compiler mustacheCompiler(
+            Mustache.TemplateLoader templateLoader,
+            Environment environment) {
+
+        MustacheEnvironmentCollector collector
+                = new MustacheEnvironmentCollector();
+        collector.setEnvironment(environment);
+
+        return Mustache.compiler()
+                .defaultValue("Some Default Value")
+                .withLoader(templateLoader)
+                .withCollector(collector);
+    }
 }
