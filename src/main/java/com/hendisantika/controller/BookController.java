ee93088518c36bfde26cf4e6d1d8f24bf49b20fe
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 07:29:07 +0700

    #9 - Add BookController

---
+++ src/main/java/com/hendisantika/controller/BookController.java
@@ -0,0 +1,33 @@
+package com.hendisantika.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-mustache-demo1
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/02/22
+ * Time: 07.28
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class BookController {
+    @GetMapping("/books")
+    public String getProducts(final Model model) {
+
+        List bookList = IntStream.range(0, 7)
+                .mapToObj(this::getBook)
+                .collect(Collectors.toList());
+
+        model.addAttribute("bookList", bookList);
+        return "book";
+    }
+}
