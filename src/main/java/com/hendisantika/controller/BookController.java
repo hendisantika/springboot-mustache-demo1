910214494b1a6e4f70b0ddda83bace35a0e96129
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 07:29:56 +0700

    #10 - Add getBook in BookController

--- src/main/java/com/hendisantika/controller/BookController.java
+++ src/main/java/com/hendisantika/controller/BookController.java
@@ -1,10 +1,10 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -23,11 +23,19 @@ public class BookController {
     @GetMapping("/books")
     public String getProducts(final Model model) {
 
-        List bookList = IntStream.range(0, 7)
+        var bookList = IntStream.range(0, 7)
                 .mapToObj(this::getBook)
                 .collect(Collectors.toList());
 
         model.addAttribute("bookList", bookList);
         return "book";
+    }
+
+    private Book getBook(int i) {
+        return new Book(i,
+                "ISBN Number -" + i,
+                "Book Name " + i,
+                "Author " + i,
+                (double) (100 * i));
     }
 }
