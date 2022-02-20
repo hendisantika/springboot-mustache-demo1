40b7c14f46e297bc84d9fc083464670882995aff
Hendi Santika <hendisantika@yahoo.co.id>
2022-02-20 07:24:33 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootMustacheDemo1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustacheDemo1Application.class, args);
+    }
+
+}
